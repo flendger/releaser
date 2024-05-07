0052ca2b3c9b48e1af2efd9a46e5ba3a9034e425
e.kiru <evgeny.kiru@v6services.ru>
2024-05-07 18:18:34 +0300

    added artifact state

---
+++ artifact/state.go
@@ -0,0 +1,36 @@
+package artifact
+
+import "time"
+
+type Status int
+
+const (
+	New Status = iota
+	BuildInProgress
+	Built
+	BuildFail
+	ReleaseInstalling
+	ReleaseInstalled
+	ReleaseFail
+	MasterInstalling
+	MasterInstalled
+	MasterFail
+	MasterTagged
+)
+
+type State struct {
+	ID                  uint64
+	ArtifactID          uint64
+	Status              Status
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	BuildInProgressAt   time.Time
+	BuiltAt             time.Time
+	BuildFailAt         time.Time
+	ReleaseInstallingAt time.Time
+	ReleaseInstalledAt  time.Time
+	ReleaseFailAt       time.Time
+	MasterInstallingAt  time.Time
+	MasterFailAt        time.Time
+	MasterTaggedAt      time.Time
+}
